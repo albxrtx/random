6775152aa98ce0599190f6f309c169fc77e12427
albxrtx <albertomariannechita@gmail.com>
2025-02-16 19:24:11 +0100

    random commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let edad = 5;
+let boton = document.querySelector("button");
+function cuentaAtras() {
+  boton.disabled = true;
+  setInterval(() => {
+    if (edad > 0) {
+      edad--;
+      document.querySelector("h1").textContent = edad;
+    } else document.querySelector("img").style.height = "400px";
+  }, 1000);
+}
